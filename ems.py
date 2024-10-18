f58cbe41b31ab24f4a6ea808f1507dc227ba8c45
Shubh Bhakuni <shubhbhakuni@gmail.com>
2024-10-18 13:01:31 +0530

    Right Column created

--- ems.py
+++ ems.py
@@ -1,5 +1,6 @@
 from customtkinter import *
 from PIL import Image
+from tkinter import ttk
 
 #The only_numbers function checks whether the input is numeric.
 def only_numbers(char):
@@ -64,7 +65,52 @@ vcmd = (leftFrame.register(only_numbers), '%S')
 salaryEntry = CTkEntry(leftFrame, font=('arial',12,'bold'), width=180, validate="key", validatecommand=vcmd)
 salaryEntry.grid(row=5,column=1,padx=20,pady=10)
 
+
 rightFrame = CTkFrame(window)
 rightFrame.grid(row=1,column=1) 
 
-window.mainloop()
+#search options
+search_options=['Id','Name','Phone','Role','Gender','Salary']
+searchBox = CTkComboBox(rightFrame, values=search_options, width=180,state='readonly')
+searchBox.grid(row=0,column=0)
+searchBox.set('Select Option')
+
+#search bar
+searchEntry = CTkEntry(rightFrame)
+searchEntry.grid(row=0,column=1)
+
+#search button
+searchButton = CTkButton(rightFrame, text='SEARCH')
+searchButton.grid(row=0,column=2)
+
+#Show Button
+showallButton = CTkButton(rightFrame, text='Show All')
+showallButton.grid(row=0,column=3,pady=5)
+
+#Treeview
+tree = ttk.Treeview(rightFrame,height=13)
+tree.grid(row=1,column=0,columnspan=4)
+
+tree['columns'] = ('Id','Name','Phone','Role','Gender','Salary')
+#headings of column
+tree.heading('Id',text='Id')
+tree.heading('Name',text='Name')
+tree.heading('Phone',text='Phone')
+tree.heading('Role',text='Role')
+tree.heading('Gender',text='Gender')
+tree.heading('Salary',text='Salary')
+
+tree.config(show='headings')
+
+tree.column('Id',width=100)
+tree.column('Name',width=160)
+tree.column('Phone',width=130)
+tree.column('Role',width=160)
+tree.column('Gender',width=100)
+tree.column('Salary',width=130)
+ 
+#styling the heading
+style = ttk.Style()
+style.configure("Treeview.Heading", font=('arial',12,'bold'))
+
+window.mainloop() 
